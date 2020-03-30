49d4cc05f0561e5e716bc10b31e03ece90e2c8da
Rahul <rahulgupta71193@gmail.com>
2020-03-30 19:14:24 +0530

    My first commit

---
+++ BasicProjectSetup/src/test/java/com/testcase/TestCase.java
@@ -0,0 +1,41 @@
+package com.testcase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.AfterTest;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class TestCase {
+ 
+
+	 public WebDriver driver;
+		
+		@BeforeTest
+		public void setUp() throws InterruptedException
+		{
+			System.setProperty("webdriver.chrome.driver","C:\\SimpleMavenProject\\chromedriver.exe");
+			driver= new ChromeDriver();
+			Thread.sleep(5000);
+		}
+		@Test(priority=1)
+		public void doLogin() throws InterruptedException
+		{
+			driver.get("https://www.google.com");
+			Thread.sleep(5000);
+			driver.manage().window().maximize();
+		}
+		@Test(priority=2)
+		public void typeInSearch()
+		{
+			//driver.get("https://www.google.com");
+			driver.findElement(By.name("q")).sendKeys("hello world");
+		}
+		@AfterTest
+		public void tearDown()
+		{
+			driver.quit();
+		}
+	}
